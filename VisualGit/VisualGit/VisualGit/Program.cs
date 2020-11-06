7ae00ea4064950d97cf0ddf61d35904deb5519a7
adrianubeda <72019874+adrianubeda@users.noreply.github.com>
2020-11-06 09:03:09 +0100

    segundo commit

---
+++ VisualGit/VisualGit/VisualGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
