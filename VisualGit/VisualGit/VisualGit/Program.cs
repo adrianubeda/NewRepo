5bcbe7c3189a078fa1c6b76abae7a070caed395d
adrianubeda <72019874+adrianubeda@users.noreply.github.com>
2020-11-06 09:53:36 +0100

    nuevo canbio

--- VisualGit/VisualGit/VisualGit/Program.cs
+++ VisualGit/VisualGit/VisualGit/Program.cs
@@ -6,7 +6,7 @@ namespace VisualGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola");
         }
     }
 }
